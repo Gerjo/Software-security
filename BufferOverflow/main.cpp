6b71a89a1a28e65b9f9123cd1fc7f46435acefc9
sparkie <dark_sparkie@hotmail.com>
2012-09-09 17:42:12 +0200

    Finished the BufferOverflow project.

--- BufferOverflow/main.cpp
+++ BufferOverflow/main.cpp
@@ -5,23 +5,51 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+void unsafe()
 {
 	srand(time(NULL));
 	int number = rand() % 10;
 	char guessedRight = '0';
 	char buffer[2];
 
-	cout << "Guess the number." << endl;
 	while(guessedRight == '0') {
-		cin >> buffer;
-		if(buffer[0] == number)
+		gets(buffer);
+
+		if(atoi(&buffer[0]) == number)
 			guessedRight = '1';
 
 		printf("Status: %c \n", guessedRight);
 	}
-	printf("Broke out of the while loop with status %s.\n", guessedRight);
+}
 
-	cin.get();
+void safe()
+{
+	srand(time(NULL));
+	int number = rand() % 10;
+	char guessedRight = '0';
+	char *buffer = new char[2];
 
+	while(guessedRight == '0') {
+
+		// NOTE: gets_s is only since recent in the C standard. (ISO/IEC 9899:2011)
+		if(gets_s(buffer, 2) == 0)
+		{
+			printf("This is safe, so stop fooling around.\n");
+		}
+
+		if(atoi(&buffer[0]) == number)
+			guessedRight = '1';
+
+		printf("Status: %c\n", guessedRight);
+	}
+
+	delete [] buffer;
+}
+
+int main(int argc, char *argv[])
+{
+	printf("Guess the number\n");
+	safe();
+	printf("Congrats, you've found it.\n");
+	system("pause");
 }
